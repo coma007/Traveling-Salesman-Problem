b6d76182b2d1991a3e5c49cb66cc37484ab5e284
Milica Sladaković <milicasladakovic7@gmail.com>
2021-12-29 13:25:51 +0100

    Odrađeno iscrtavanje.
    Tačke i linije

--- app/other.py
+++ app/other.py
@@ -7,4 +7,4 @@ def read_file(filename):
     with open(filename, "r") as f:
         for line in f.readlines():
             data = line.split()
-            c.POINTS.append(Point(data[1], data[2]))
+            c.POINTS.append(Point(float(data[1]), float(data[2])))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.other import *
+from app.gui import *
 
 if __name__ == '__main__':
 
     read_file(c.FILENAME)
-
+    play()
