e09c38e0cf999198a44f5bcc0d54a836ae825a4b
Nemanja Dutina <dutina.sv27.2020@uns.ac.rs>
2021-12-29 13:54:23 +0100

    Formiranje prve generacije

---
+++ app/ga.py
@@ -0,0 +1,29 @@
+from app.other import *
+import random
+from copy import deepcopy
+
+
+def create_first_generation(population_number):
+    generation = []
+    for i in range(population_number):
+        population = deepcopy(c.POINTS)
+        random.shuffle(population)
+        population.append(population[0])
+        generation.append(population)
+    return generation
+
+
+def calculate_fitness(generation):
+    total_fitness = 0
+    sum_distance = 0
+    normalize_fitness = []
+    fitness = []
+    for population in generation:
+        for i in range(len(population)-1):
+            sum_distance += distance(population[i], population[i+1])
+        fitness.append(1/sum_distance)
+        total_fitness += 1/sum_distance
+    for d in fitness:
+        normalize_fitness.append(d/total_fitness)
+    return normalize_fitness
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from app.other import *
+from app.ga import *
 
 if __name__ == '__main__':
 
     read_file(c.FILENAME)
-
+    generation = create_first_generation(100)
+    generation_fitness = calculate_fitness(generation)
+    print(generation_fitness)
